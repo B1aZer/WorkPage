fe0ef8f39e306a6368c174d59096c482a194b1bb
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2013-04-12 13:05:17 +0400

    some fixes

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,8 +1,23 @@
 $(document).ready( function() {
-	var ths = document.querySelectorAll("#table2 th");
-	var tds = document.querySelectorAll("#table2 td");
+    var merge = function (obj1, obj2) {
+        var res = [];
+        for (var i=0; i<obj1.length; i++) {
+            res[i] = obj1[i];
+        }
+        for (var i=0; i<obj2.length; i++) {
+            res[obj1.length+i] = obj2[i];
+        }
+        return res
+    }
+	var table = document.getElementById("table2");
+	var ths = table.getElementsByTagName("th");
+	var tds = table.getElementsByTagName("td");
+    console.log(tds);
 	/* assuming we have valid number if cells */
-	var all = document.querySelectorAll("#table2 th, #table2 td");
+	//var allm = document.querySelectorAll("#table2 th, #table2 td");
+	var all = merge(ths, tds);
+    console.log(all);
+    console.log(allm);
 	var rows = ths.length;
 	var colls = all.length / ths.length;
 	/* all cells have equal width in thix example */
